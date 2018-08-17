ec7daef9d4f29f95de712618a38533955b5b168e
Kocka1235 <42372263+Kocka1235@users.noreply.github.com>
2018-08-17 16:36:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,16 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const dispatcher = connection.playFile('C:/Users/Discord/Desktop/myfile.mp3');
+const Bot = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
-
-  if (message.content === '/join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
+bot.on('message', function(message){
+  if(message.content == 'Hello')
+  {
+    message.reply('Hello, how are you?');
   }
 });
 
+bot.on('ready', function(){
+  console.log("Ready");
+})
 
+bot.login(process.env.BOT_TOKEN);
 
-client.login(process.env.BOT_TOKEN);
