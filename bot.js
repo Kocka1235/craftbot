b3005179adbbfec19f5693716d306cc9c7574b33
Kocka1235 <42372263+Kocka1235@users.noreply.github.com>
2018-08-18 17:08:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,9 @@
-const Discord = require('discord.js');
-const Bot = new Discord.Client();
+const Discord= require('Discord.js');
+const Bot= new Discord.Client();
 
 bot.on('message', function(message){
-  if(message.content == 'Hello')
-  {
-    message.reply('Hello, how are you?');
+  if(message.content == 'Hello'){
+  message.reply('Hello, how are you?');
   }
 });
-
-bot.on('ready', function(){
-  console.log("Ready");
-})
-
-bot.login(process.env.BOT_TOKEN);
 
